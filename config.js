30a66ef2e7a27792b3902b874618cf044cd4c9a3
Kristabell Johnson <bell77iam@gmail.com>
2021-03-02 20:43:22 -0700

    changes to Package Json and nodes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
   secret: 'MY SECRET',
-  DB_URI_DEV: 'mongodb://mongodb0.example.com:27017/admin',
+  DB_URI_DEV: 'mongodb://localhost/demo',
   DB_URI: 'mongodb://mongodb0.example.com:27017/admin'
 };
