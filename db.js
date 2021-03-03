e26df63f571b21ae134d2e39e2baeaac84b15a7f
Kristabell Johnson <bell77iam@gmail.com>
2021-03-02 22:16:27 -0700

    db.js

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const url = `mongodb+srv://root:root@cluster0.nfkwi.mongodb.net/Cluster0?retryWrites=true&w=majority`;
+
+const connectionParams = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}
+mongoose.connect(url, connectionParams)
+    .then(() => {
+        console.log('Connected to database ')
+    })
+    .catch((err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
