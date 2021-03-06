a7412aa7a99bc7db45ea3170de1032eecdc1ad03
Evan Gerweck <gerweckevan@gmail.com>
2021-03-05 19:41:54 -0700

    heroku

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-
 module.exports = {
-  secret: 'MY SECRET',
-  DB_URI_DEV: 'mongodb://localhost/demo',
-  DB_URI: 'mongodb://mongodb0.example.com:27017/admin'
+  secret: "MY SECRET",
+  DB_URI_DEV: "mongodb://localhost/demo",
+  DB_URI:
+    "mongodb+srv://root:root@cluster0.nfkwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
--- db.js
+++ db.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const url = process.env.ATLAS_URI`mongodb+srv://root:root@cluster0.nfkwi.mongodb.net/Cluster0?retryWrites=true&w=majority`;
+const url = `mongodb+srv://root:root@cluster0.nfkwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const connectionParams = {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-}
-mongoose.connect(url, connectionParams)
-    .then(() => {
-        console.log('Connected to database ')
-    })
-    .catch((err) => {
-        console.error(`Error connecting to the database. \n${err}`);
-    })
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+mongoose
+  .connect(url, connectionParams)
+  .then(() => {
+    console.log("Connected to database ");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  });
