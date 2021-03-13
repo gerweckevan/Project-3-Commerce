b746ad1f8fb5a26549d9f21d40708bb9903947bc
Michael Hutchings <MHUTCHBOT@GMAIL.COM>
2021-03-13 11:35:59 -0700

    first commit

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  secret: "admin",
-  DB_URI_DEV: "mongodb://localhost/demo",
-  DB_URI:
-    "mongodb+srv://root:root@cluster0.nfkwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-};
